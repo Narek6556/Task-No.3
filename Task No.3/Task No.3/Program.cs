68742d61643775f992add60c9905c35f6f860563
Narek6556 <52848075+Narek6556@users.noreply.github.com>
2019-08-07 18:14:23 +0400

    Add files via upload

---
+++ Task No.3/Task No.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Task_No._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Write("Enter first number...  ");
+            int firstNumber = int.Parse(ReadLine());
+            Write("Enter second number...  ");
+            int secondNumber = int.Parse(ReadLine());
+            firstNumber = firstNumber + secondNumber;
+            secondNumber = firstNumber - secondNumber;
+            firstNumber = firstNumber - secondNumber;
+            WriteLine("First number: {0} ", firstNumber);
+            WriteLine("Second number: {0}", secondNumber);
+
+
+        }
+    }
+}
